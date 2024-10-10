9f31384a7d99d0136a099efce218b7b3a42bcb3c
byhturk <byhturk@gmail.com>
2024-10-10 20:19:48 +0300

    Bildirim ekranı Sıralaması Değiştirildi

--- src/main/java/org/traccar/model/Event.java
+++ src/main/java/org/traccar/model/Event.java
@@ -40,33 +40,34 @@ public class Event extends Message {
 
     public static final String ALL_EVENTS = "allEvents";
 
-    public static final String TYPE_COMMAND_RESULT = "commandResult";
+    public static final String TYPE_ALARM = "alarm";
+
+    public static final String TYPE_GEOFENCE_ENTER = "geofenceEnter";
+    public static final String TYPE_GEOFENCE_EXIT = "geofenceExit";
+
+    public static final String TYPE_IGNITION_ON = "ignitionOn";
+    public static final String TYPE_IGNITION_OFF = "ignitionOff";
+
+    public static final String TYPE_DEVICE_OVERSPEED = "deviceOverspeed";
+    public static final String TYPE_DEVICE_MOVING = "deviceMoving";
+    public static final String TYPE_DEVICE_STOPPED = "deviceStopped";
+
+    public static final String TYPE_MAINTENANCE = "maintenance";
 
     public static final String TYPE_DEVICE_ONLINE = "deviceOnline";
     public static final String TYPE_DEVICE_UNKNOWN = "deviceUnknown";
     public static final String TYPE_DEVICE_OFFLINE = "deviceOffline";
     public static final String TYPE_DEVICE_INACTIVE = "deviceInactive";
-    public static final String TYPE_QUEUED_COMMAND_SENT = "queuedCommandSent";
 
-    public static final String TYPE_DEVICE_MOVING = "deviceMoving";
-    public static final String TYPE_DEVICE_STOPPED = "deviceStopped";
-
-    public static final String TYPE_DEVICE_OVERSPEED = "deviceOverspeed";
     public static final String TYPE_DEVICE_FUEL_DROP = "deviceFuelDrop";
     public static final String TYPE_DEVICE_FUEL_INCREASE = "deviceFuelIncrease";
-
-    public static final String TYPE_GEOFENCE_ENTER = "geofenceEnter";
-    public static final String TYPE_GEOFENCE_EXIT = "geofenceExit";
-
-    public static final String TYPE_ALARM = "alarm";
-
-    public static final String TYPE_IGNITION_ON = "ignitionOn";
-    public static final String TYPE_IGNITION_OFF = "ignitionOff";
-
-    public static final String TYPE_MAINTENANCE = "maintenance";
-    public static final String TYPE_TEXT_MESSAGE = "textMessage";
+    
     public static final String TYPE_DRIVER_CHANGED = "driverChanged";
+
+    public static final String TYPE_TEXT_MESSAGE = "textMessage";
     public static final String TYPE_MEDIA = "media";
+    public static final String TYPE_COMMAND_RESULT = "commandResult";
+    public static final String TYPE_QUEUED_COMMAND_SENT = "queuedCommandSent";
 
     private Date eventTime;
 
